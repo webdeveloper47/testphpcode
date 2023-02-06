b5a1421792a6588857901ac2564087c02cfb6e96
miraj <mirajulislam0724@gmail.com>
2023-02-06 11:48:12 +0600

    finish

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
-// Assignment 02
+
 
 <?php
 
+// Assignment 02
 
 $tutionFee =22000;
+$comision1= $tutionFee * 25/100;
+$comision2= $tutionFee * 20/100;
+$comision3= $tutionFee * 15/100;
 
-$tutionFee = (($tutionFee >20000)?("Comision 25% "):(($tutionFee>10000 && $tutionFee<20000)?("Comision 20%"):(($tutionFee>7000 && $tutionFee<10000)?("Comision 15%"):("The data will be invalid"))));
+$tutionFee = (($tutionFee >20000)?($comision1):(($tutionFee>10000 && $tutionFee<20000)?($comision2):(($tutionFee>7000 && $tutionFee<10000)?($comision3):("The data will be invalid"))));
 
 
 echo $tutionFee;
